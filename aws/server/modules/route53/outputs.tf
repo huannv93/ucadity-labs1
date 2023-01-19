cb774e615e6b373b60904937f48bc964073d157c
huannv93 <huannguyen7651@gmail.com>
2023-01-19 14:53:54 +0700

    update code

--- aws/server/modules/route53/outputs.tf
+++ aws/server/modules/route53/outputs.tf
@@ -1,3 +1,7 @@
+# output "app_route53_record" {
+#   value = aws_route53_record.app.name
+# }
+###them hostzone de output show full subdomain route53
 output "app_route53_record" {
-  value = aws_route53_record.app.name
+  value = "${aws_route53_record.app.name}.${var.hosted_zone}"
 }
