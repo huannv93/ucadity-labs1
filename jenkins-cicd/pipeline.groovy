041ef707a54c2ee4c49f68bc286eb1e8dc0079c2
huannv93 <huannguyen7651@gmail.com>
2023-11-09 11:15:05 +0700

    update jenkinscicd

--- jenkins-cicd/pipeline.groovy
+++ jenkins-cicd/pipeline.groovy
@@ -2,20 +2,42 @@ pipeline {
     agent any
     
     stages {
+
+        stage('Checkout') {
+            steps {
+                script {
+                    checkout scm: [
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        userRemoteConfigs: [[url: 'https://github.com/huannv93/ucadity-labs1.git']]
+                    ]
+                }
+            }
+        }
+
         stage('Build Frontend') {
             agent {
                 docker { image 'circleci/node:13.8.0' }
             }
             steps {
-                checkout()
+
                 script {
-                    def frontendCacheKey = 'frontend-build'
-                    withCache(frontendCacheKey) {
-                        dir('frontend') {
-                            sh 'npm install'
-                            sh 'npm run build'
-                        }
+                    checkout scm: [
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        userRemoteConfigs: [[url: 'https://github.com/huannv93/ucadity-labs1.git']]
+                    ]
+                    // sh 'cd frontend'
+                    // sh 'npm install'
+                    // sh 'npm run build'
+                    // def frontendCacheKey = 'frontend-build'
+                    // withCache(frontendCacheKey) {
+                    dir('frontend') {
+                        sh 'sudo chown -R 992:992 "/.npm"'
+                        sh 'sudo npm install'
+                        sh 'sudo npm run build'
                     }
+                    // }
                 }
             }
         }
