a574855164c8c8a8964e08d5908a239e9db2f18d
huannv93 <huannguyen7651@gmail.com>
2023-11-11 17:09:23 +0700

    hello

--- jenkins-cicd/app.groovy
+++ jenkins-cicd/app.groovy
@@ -169,7 +169,7 @@ pipeline {
         // }
 // Comment this code to pass scan security
 
-        stage('checkout') {
+        stage('Checkout') {
             steps {
                 script {
                     checkout scm: [
@@ -182,65 +182,65 @@ pipeline {
             }
         }
 
-        // stage('Build and Push Image Backend') {
-        //     steps {
-        //         script {
-        //             // Get the Git commit hash
-        //             def commitHash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+        stage('Build and Push Image Backend ECR Repo') {
+            steps {
+                script {
+                    // Get the Git commit hash
+                    def commitHash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 
-        //             // Build Docker image with Git commit hash as the tag
-        //             dir('backend') {
-        //                 sh "docker build -t ${DOCKER_IMAGE_NAME}:${commitHash} ."
-        //                 sh 'docker image ls'
-        //                 // Login ECR
-        //                 sh 'aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 221008696644.dkr.ecr.ap-northeast-1.amazonaws.com'
-        //                 // Push Image to ECR
-        //                 sh "docker push ${DOCKER_IMAGE_NAME}:${commitHash} "
-        //             }
-        //         }
-        //     }
-        // }
+                    // Build Docker image with Git commit hash as the tag
+                    dir('backend') {
+                        sh "docker build -t ${DOCKER_IMAGE_NAME}:${commitHash} ."
+                        sh 'docker image ls'
+                        // Login ECR
+                        sh 'aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 221008696644.dkr.ecr.ap-northeast-1.amazonaws.com'
+                        // Push Image to ECR
+                        sh "docker push ${DOCKER_IMAGE_NAME}:${commitHash} "
+                    }
+                }
+            }
+        }
 
-        // stage('Update ECS Task Definition and ECS Service') {
-        //     steps {
-        //         script {
-        //             // Get the Git commit hash or another identifier
-        //             def commitHash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+        stage('Update ECS Task Definition and ECS Service') {
+            steps {
+                script {
+                    // Get the Git commit hash or another identifier
+                    def commitHash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
         
-        //             // Describe the existing ECS Task Definition
-        //             def TASK_DEFINITION = sh(script: "aws ecs describe-task-definition --task-definition ${TASK_DEFINITION_NAME} --region ${AWS_DEFAULT_REGION} --output json", returnStdout: true).trim()
+                    // Describe the existing ECS Task Definition
+                    def TASK_DEFINITION = sh(script: "aws ecs describe-task-definition --task-definition ${TASK_DEFINITION_NAME} --region ${AWS_DEFAULT_REGION} --output json", returnStdout: true).trim()
                 
-        //             // Update the Docker image tag in the Task Definition
-        //             def NEW_TASK_DEFINITION = sh(script: "echo '${TASK_DEFINITION}' | jq --arg IMAGE ${DOCKER_IMAGE_NAME}:${commitHash} '.taskDefinition.containerDefinitions[0].image = \$IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) |  del(.registeredAt)  | del(.registeredBy)'", returnStdout: true).trim()
+                    // Update the Docker image tag in the Task Definition
+                    def NEW_TASK_DEFINITION = sh(script: "echo '${TASK_DEFINITION}' | jq --arg IMAGE ${DOCKER_IMAGE_NAME}:${commitHash} '.taskDefinition.containerDefinitions[0].image = \$IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) |  del(.registeredAt)  | del(.registeredBy)'", returnStdout: true).trim()
                 
-        //             def container_definitions = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.containerDefinitions' -c", returnStdout: true).trim()
+                    def container_definitions = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.containerDefinitions' -c", returnStdout: true).trim()
         
-        //             // Get info to create new ECS Task Definition
+                    // Get info to create new ECS Task Definition
 
-        //             def requires_compatibilities = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq -r '.taskDefinition.requiresCompatibilities[0]' -c", returnStdout: true).trim()
-        //             def network_mode = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.networkMode' -c", returnStdout: true).trim()
+                    def requires_compatibilities = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq -r '.taskDefinition.requiresCompatibilities[0]' -c", returnStdout: true).trim()
+                    def network_mode = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.networkMode' -c", returnStdout: true).trim()
 
-        //             def task_role_arn = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.taskRoleArn' -c", returnStdout: true).trim()
+                    def task_role_arn = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.taskRoleArn' -c", returnStdout: true).trim()
 
-        //             def execution_role_arn = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.executionRoleArn' -c", returnStdout: true).trim()
-        //             def cpu = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.cpu' -c", returnStdout: true).trim()
-        //             def memory = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.memory' -c", returnStdout: true).trim()
+                    def execution_role_arn = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.executionRoleArn' -c", returnStdout: true).trim()
+                    def cpu = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.cpu' -c", returnStdout: true).trim()
+                    def memory = sh(script: "echo '${NEW_TASK_DEFINITION}' | jq '.taskDefinition.memory' -c", returnStdout: true).trim()
               
         
-        //             // Register a new revision of the ECS Task Definition
-        //             def NEW_TASK_INFO = sh(script: "aws ecs register-task-definition --family CloudSM --container-definitions '${container_definitions}' --requires-compatibilities ${requires_compatibilities}  --network-mode ${network_mode}  --task-role-arn ${task_role_arn} --execution-role-arn ${execution_role_arn} --cpu ${cpu} --memory ${memory} --region ${AWS_DEFAULT_REGION}", returnStdout: true).trim()
+                    // Register a new revision of the ECS Task Definition
+                    def NEW_TASK_INFO = sh(script: "aws ecs register-task-definition --family CloudSM --container-definitions '${container_definitions}' --requires-compatibilities ${requires_compatibilities}  --network-mode ${network_mode}  --task-role-arn ${task_role_arn} --execution-role-arn ${execution_role_arn} --cpu ${cpu} --memory ${memory} --region ${AWS_DEFAULT_REGION}", returnStdout: true).trim()
         
-        //             // Get the new revision number
-        //             def NEW_REVISION = sh(script: "echo '${NEW_TASK_INFO}' | jq '.taskDefinition.revision'", returnStdout: true).trim()
+                    // Get the new revision number
+                    def NEW_REVISION = sh(script: "echo '${NEW_TASK_INFO}' | jq '.taskDefinition.revision'", returnStdout: true).trim()
 
-        //             // Update ecs service
-        //             sh "aws ecs update-service --region '${AWS_DEFAULT_REGION}' --cluster '${ECS_CLUSTER}' --service '${ECS_SERVICE}' --task-definition '${TASK_DEFINITION_NAME}':'${NEW_REVISION}' --force-new-deployment"
+                    // Update ecs service
+                    sh "aws ecs update-service --region '${AWS_DEFAULT_REGION}' --cluster '${ECS_CLUSTER}' --service '${ECS_SERVICE}' --task-definition '${TASK_DEFINITION_NAME}':'${NEW_REVISION}' --force-new-deployment"
 
         
-        //                             }
-        //     }
+                                    }
+            }
 
-        // }
+        }
 
         stage('Deploy FrontEnd') {
             steps {
